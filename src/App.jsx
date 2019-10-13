6981ef2fb62e7efc7cdbda64a03d1595ddfa8ea8
Zalinafali <38260620+Zalinafali@users.noreply.github.com>
2019-10-13 20:58:36 +0200

    Production bundle analysis

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const MyApp = () => (
-  <h1>Minimal React zalinafali</h1>  
+  <h1>
+    Minimal React zalinafali
+    <p>
+      Bundle size: 129K bytes, 
+      Load time of the bundle: 45 ms, 
+      Last commit SHA1: 0f59bee89caa64d69da9fcc6149bfd26f9481a66
+    </p>
+  </h1>  
 )
 
 export default MyApp
