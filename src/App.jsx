0318a44599cf3718ab1acb8d9f5dfb917973c3ea
Zalinafali <38260620+Zalinafali@users.noreply.github.com>
2019-10-15 22:51:17 +0200

    Lab1Ex5

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ const generateArray = (n) => (Array.from({length: n}, (v,i)=>i+1))
 
 const generateRandomArray = (n) => (Array.from({length: n}, () => Math.floor(Math.random() * 25)+1))
 
+const Arr1 = [2,56,23,88,17,4];
+const Arr2 = [2,5,8,10];
+
+Arr2.sqrt = function(){
+  return Arr2.map(element => Math.sqrt(element));
+}
+
+const sqrtArr2 = () => (Array.from(Arr2, element => Math.sqrt(element)))
+
 const MyApp = () => (
   <div>
     <h1>
@@ -21,6 +30,15 @@ const MyApp = () => (
 
       <p>generate an array of random integers from 1 to 25 (i.e. 12)</p>
       {generateRandomArray(12).map(element => <li>{element}</li>)}
+
+      <p>Print all elements larger than 15</p>
+      {Arr1.map(element => element>15?<li>{element}</li>:false)}
+
+      <p>Sqrt of Arr2 (method for Arr2)</p>
+      {Arr2.sqrt().map(element => <li>{element}</li>)}
+
+      <p>Sqrt of Arr2 (function)</p>
+      {sqrtArr2().map(element => <li>{element}</li>)}
 
     </ul>
   </div>
