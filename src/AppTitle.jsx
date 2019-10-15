e6c03b589d493edcf27e097bd15d45ced6c9f0b7
Zalinafali <38260620+Zalinafali@users.noreply.github.com>
2019-10-15 20:17:44 +0200

    Lab1Ex1

---
+++ src/AppTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppTitle = () => (
+  <h1>
+    AppTitle test
+  </h1>  
+)
+
+export default AppTitle
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
 import MyApp from './App'
+import AppTitle from './AppTitle'
 
-render(<MyApp />, document.getElementById('reactinjectedhere'))
+render(
+    <div>
+        <MyApp />,
+        <AppTitle />
+    </div>,
+    document.getElementById('reactinjectedhere')
+)
