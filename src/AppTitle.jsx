5626c54b240a19b7d8c799a7fd736ac8791769ba
Zalinafali <38260620+Zalinafali@users.noreply.github.com>
2019-10-22 21:37:33 +0200

    Lab2Ex2

--- src/AppTitle.jsx
+++ src/AppTitle.jsx
@@ -5,60 +5,53 @@ const generateArray = (x,y) => Array.from({length: (y-x+1)}, (v,k)=> x+k);
 class AppTitle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {a: '', b: '', arr: []};
+    this.state = {a: '', b: '', arr: [], renderCounter: 0};
 
     this.handleChangeA = this.handleChangeA.bind(this);
     this.handleChangeB = this.handleChangeB.bind(this);
-    this.handleSubmitA = this.handleSubmitA.bind(this);
-    this.handleSubmitB = this.handleSubmitB.bind(this);
   } 
 
   handleChangeA(event) {
-    this.setState({a: Number(event.target.value)});
+    this.setState({a: Number(event.target.value)}, (nextState) => {
+      if(this.state.a > 0 && this.state.b > this.state.a)
+        this.setState({arr: generateArray(Number(this.state.a),Number(this.state.b)).map(x => " " + x)});
+      else
+        this.setState({arr: []});
+    });
+    { console.log('New number a: ' + event.target.value) }
   }
 
   handleChangeB(event) {
-    this.setState({b: Number(event.target.value)});
-  }
-  
-  handleSubmitA(event) {
-    { console.log('New number a: ' + this.state.a) }
-    if(this.state.a > 0 && this.state.b > this.state.a)
+    this.setState({b: Number(event.target.value)}, (nextState) => {
+      if(this.state.a > 0 && this.state.b > this.state.a)
       this.setState({arr: generateArray(Number(this.state.a),Number(this.state.b)).map(x => " " + x)});
     else
       this.setState({arr: []});
-    event.preventDefault();
-  }
-
-  handleSubmitB(event) {
-    { console.log('New number b: ' + this.state.b) }
-    if(this.state.a > 0 && this.state.b > this.state.a)
-      this.setState({arr: generateArray(Number(this.state.a),Number(this.state.b)).map(x => " " + x)});
-    else
-      this.setState({arr: []});
-    event.preventDefault();
+    });
+    { console.log('New number b: ' + event.target.value) }
   }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmitA}>
-          <label>
+        
+        
+          <label for="a">
             a:
-            <input type="text" pattern="[0-9]*" value={this.state.a} onChange={this.handleChangeA} />
+            <input type="text" id="a" pattern="[0-9]*" value={this.state.a} onChange={this.handleChangeA} />
           </label>
-          <input type="submit" value="Send" />
-        </form>
-        <form onSubmit={this.handleSubmitB}>
-          <label>
+         
+      
+          <label for="b">
             b:
-            <input type="text" pattern="[0-9]*" value={this.state.b} onChange={this.handleChangeB} />
+            <input type="text" id="b" pattern="[0-9]*" value={this.state.b} onChange={this.handleChangeB} />
           </label>
-          <input type="submit" value="Send" />
-        </form>
+
+       
         <label>
           Generated array: {this.state.arr}
         </label>
+        
       </div>
     );
   }
